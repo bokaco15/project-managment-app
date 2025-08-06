c0f1be8b07a9c5d5bd6386b737aa311b78b24811
bokaco15 <bborislavilic@gmail.com>
2025-08-06 14:53:17 +0200

    * Made main layout navbar and footer

---
+++ resources/views/Components/footer.blade.php
@@ -0,0 +1,3 @@
+<div class="footer bg-dark">
+    <p class="text-center text-light p-3">&copy;{{date('Y')}} Borislav Ilic</p>
+</div>
---
+++ resources/views/Components/navbar.blade.php
@@ -0,0 +1,17 @@
+<div class="nav bg-dark">
+
+    <div class="container pt-4 pb-4 d-flex justify-content-between">
+        <div class="navHeading">
+            <a href="/" class="text-primary text-decoration-none fst-italic">Project Managment App</a>
+        </div>
+        <div class="links">
+            <a href="/" class="text-light p-1 text-decoration-none">Home</a>
+            <a href="/" class="text-light p-1 text-decoration-none">Home</a>
+            <a href="/" class="text-light p-1 text-decoration-none">Home</a>
+            <a href="/" class="text-light p-1 text-decoration-none">Home</a>
+            <a href="/" class="text-light p-1 text-decoration-none">Home</a>
+        </div>
+    </div>
+
+    </div>
+</div>
